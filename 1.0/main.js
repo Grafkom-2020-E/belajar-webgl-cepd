64b3a2765c876cb120c80799669225200409fa80
cepd <62136051+cepd@users.noreply.github.com>
2020-10-20 20:10:17 +0800

    Update main.js
    segitiga dengan gl.LINE_LOOP

--- 1.0/main.js
+++ 1.0/main.js
@@ -4,10 +4,36 @@ function main() {
     var gl = canvas.getContext("webgl");
     //gl ini kayak alat tulis, canvas kertasnya
 
+    // cara lain, jadi code untuk 2 dibawah ini ditaroh di html
+    // var vertexShaderSource = document.getElementById("vertexShaderSource").text
+    // var vertexShaderSource = document.getElementById("fragmentShaderSource").text
+
+    //PERTEMUAN 3
+    //MEMBUAT 3 TITIK
+    /* Definisi data vertex
+    * TITIK A (-0.5,-0.5)
+    * TITIK B (0.5, -0.5)
+    * TITIK C (0.5, 0.5)
+    */
+
+    var vertices = [
+        -0.5,-0.5,          //titik A
+        0.5, -0.5,          //titik B
+       -0.5, 0.5           //titik C
+    ];
+
+
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); //menyimpan alamat
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices),gl.STATIC_DRAW) //masukkan datanya
+    //bilang kepada GPU biar ngelepas buffernya
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
     var vertexShaderSource = `
+    attribute vec2 a_Position;
     void main(){
-        gl_PointSize = 80.0;
-        gl_Position = vec4(0.0, 0.2, 0.0, 1.0);
+        gl_PointSize = 25.0;
+        gl_Position = vec4(a_Position, 0.0, 1.0);
         
     }
     `;
@@ -57,11 +83,30 @@ function main() {
     //mulai menggunakan konteks 
     gl.useProgram(shaderProgram);
 
+    /* 
+    * Ajarkan attribute a_position di GPU 
+    * tentang pengambilan data vertex dari ARRAY_BUFFER
+    * awali dengan membuat pointer
+    */
+   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+   var aPositionLoc = gl.getAttribLocation(shaderProgram, "a_Position");
+   gl.vertexAttribPointer(aPositionLoc, 2 , gl.FLOAT, false, 0, 0);
+   //size -> berapa size yang ingin ditarik
+   // type -> tipe data, float (by default vec2 ada 2 float)
+   // normalize -> false; kapan pake true? ketika ga pake 0 sampe 1
+   // stride -> isi nol dulu ntar dijelasin
+   // offset -> -"-
+    gl.enableVertexAttribArray(aPositionLoc);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
     //mulai mewarnaiiii
     gl.clearColor(0.0, 0.0, 0.0, 1.0); //RGBA
     gl.clear(gl.COLOR_BUFFER_BIT);
 
 
-    //buat apa ini ketinggalan T-T
-    gl.drawArrays(gl.POINTS, 0, 1);
+
+  var primitive = gl.LINE_LOOP;
+  var offset = 0;
+  var nVertex = 3;
+  gl.drawArrays(primitive, offset, nVertex);
 }
